e71dc926f56fc6c68641ffcfc5d84227de204df2
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2019-08-08 19:04:12 +0200

    Update addon.py

--- addon.py
+++ addon.py
@@ -7,26 +7,24 @@ import RPi.GPIO as GPIO
 
 # Set pin number once
 RELAY_PIN = 18
+counter = 0
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(RELAY_PIN, GPIO.OUT)
 
+
+while counter > 3
 # read the current state of the pin
-current_state = GPIO.input(RELAY_PIN)
-ssaver_state = xbmc.getCondVisibility("System.ScreenSaverActive")
-
-if ssaver_state == True:
-
+    current_state = GPIO.input(RELAY_PIN)
+    ssaver_state = xbmc.getCondVisibility("System.ScreenSaverActive")
+    if ssaver_state == True:
     # Screensaver is on so we should turn off the relay
     GPIO.output(RELAY_PIN, GPIO.LOW)
     state = "off"
-
-else:
-
+    else:
     # Screensaver is off so we should turn on the relay
     GPIO.output(RELAY_PIN, GPIO.HIGH)
     state = "on"
-
-# Send notification
-xbmcgui.Dialog().notification("Relay", "Amplifier is now {0}!".format(state))
+    # Send notification
+    xbmcgui.Dialog().notification("Relay", "Amplifier is now {0}!".format(state))
